31391e7789f5dd1bdb352a770d6e9770e2c0327d
Victor Pino <victopin0@gmail.com>
2017-05-24 13:18:35 -0400

    feat(expenses): More functions to services

--- expenses/src/app/home/home.component.ts
+++ expenses/src/app/home/home.component.ts
@@ -10,13 +10,24 @@ import { ClothesService } from '../services/clothes.service';
 export class HomeComponent{
 
 	public title:string;
+	public list_clothes:Array<string>;
+	public garment_save:string;
 
 	constructor(private _clothesService:ClothesService){
 		this.title = 'Main page';
 	}
 
 	ngOnInit(){
-		console.log(this._clothesService.test('Cowboy pants'));
+		this.list_clothes = this._clothesService.getClothes();
+	}
+
+	saveGarment(){
+		this._clothesService.addClothes(this.garment_save);
+		this.garment_save = null;
+	}
+
+	deleteGarment(index:number){
+		this._clothesService.deleteGarment(index);
 	}
 	
 }
--- expenses/src/app/services/clothes.service.ts
+++ expenses/src/app/services/clothes.service.ts
@@ -3,10 +3,28 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ClothesService{
 
-	public name_garment:string;
-	
-	test(name_garment){
-		return name_garment;
+	public garment:string;
+	public collection =['Jeans White', 'Shirt red'];
+
+	test(garment){
+		return garment;
 	}
-	
+
+	addClothes(garment:string):Array<string>{
+
+		this.collection.push(garment);
+
+		return this.getClothes();
+	}	
+
+	deleteGarment(index:number){
+		this.collection.slice(1, 1);
+
+		return this.getClothes();
+	}
+
+	getClothes():Array<string>{
+		return this.collection;
+	}
+
 }
